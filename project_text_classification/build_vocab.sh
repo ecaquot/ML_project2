fcaa321b17c95c85e7ddbc5540b3cc9897c1a07b
ecaquot <etienne.caquot@epfl.ch>
2019-11-20 08:33:06 +0100

    Run given scipts

--- project_text_classification/build_vocab.sh
+++ project_text_classification/build_vocab.sh
@@ -2,4 +2,4 @@
 
 # Note that this script uses GNU-style sed. On Mac OS, you are required to first
 #    brew install gnu-sed --with-default-names
-cat pos_train.txt neg_train.txt | sed "s/ /\n/g" | grep -v "^\s*$" | sort | uniq -c > vocab.txt
+cat Datasets/twitter-datasets/train_pos.txt Datasets/twitter-datasets/train_neg.txt | sed "s/ /\n/g" | grep -v "^\s*$" | sort | uniq -c > vocab.txt
